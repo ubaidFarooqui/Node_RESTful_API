2c99e59fb4565262fdd7615d52aeb69065486334
Ubaid Ahmed Farooqui <farooqui.ubaid@gmail.com>
2018-06-28 02:51:51 +0200

    Setup App for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-    
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://<ubaidFarooqui>:<Ubaid1987>@ds121251.mlab.com:21251/todoapp1'
-}
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+//let db = {
+//    
+//    localhost: 'mongodb://localhost:27017/TodoApp',
+//    mlab: 'mongodb://<ubaidFarooqui>:<Ubaid1987>@ds121251.mlab.com:21251/todoapp1'
+//}
+//mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports ={mongoose};
