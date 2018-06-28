bea2d9e0558bbe4a1bcb17e0fd0838bc4402ebf2
Ubaid Ahmed Farooqui <farooqui.ubaid@gmail.com>
2018-06-28 03:16:24 +0200

    Setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 //let db = {
 //    
 //    localhost: 'mongodb://localhost:27017/TodoApp',
-//    mlab: 'mongodb://<ubaidFarooqui>:<Ubaid1987>@ds121251.mlab.com:21251/todoapp1'
+//    mlab: 'mongodb://ubaidFarooqui:Ubaid1987@ds121251.mlab.com:21251/todoapp1'
 //}
 //mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
