8688dea1c6c1c012fc01ea2262cb8caf9c1f8487
Ubaid Ahmed Farooqui <farooqui.ubaid@gmail.com>
2018-07-01 00:10:38 +0200

    Add Test PATCH /todo/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -14,7 +14,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id : new ObjectID(),
-    text: 'Second tst todo'
+    text: 'Second tst todo',
+    completed: true,
+    completedAt: 333 //for testing PATCH route 
 }];
 
 
@@ -152,7 +154,46 @@ describe('DELETE /todo/:id', () => {
     });
 });
 
-
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexID = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+        
+        request(app)
+        .patch(`/todos/${hexID}`)
+        .send({
+            completed: true,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            expect(res.body.todo.completedAt).toBeA('number');
+        })
+        .end(done)
+    });
+    
+    it('should clear completedAt when todo is not completed', (done) => {
+        
+        var hexID = todos[1]._id.toHexString();
+        var text = 'This should be the new text!!';
+        
+        request(app)
+        .patch(`/todos/${hexID}`)
+        .send({
+            completed: false,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toNotExist();
+        })
+        .end(done)
+    });
+});
 
 
 
